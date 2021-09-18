2d0427ed242bc96fa0d1e97ada252f204bdc9bc6
Wu <134@xxx.com>
2021-09-18 11:20:13 +0800

    four commit

--- src/test/java/test/test1.java
+++ src/test/java/test/test1.java
@@ -6,6 +6,7 @@ public class test1 {
         System.out.println("hello git222");
         System.out.println("hello git333");
         System.out.println("hello githot-fix");
+        System.out.println("hello githot-fixmaster");
 
 
     }
